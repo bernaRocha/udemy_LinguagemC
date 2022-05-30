fa2c3237b2b876025498ab8d54623af6f91576b6
Bernardo <mrochabernardo@gmail.com>
2022-05-30 09:12:16 -0300

    Ex conversão Fahrenheit para Celsius

--- 01_variaveisExercios/main.c
+++ 01_variaveisExercios/main.c
@@ -6,29 +6,29 @@ int main()
 {
     setlocale(LC_ALL, "");
 
-/*    printf("Ex1, pedir um número inteiro e imprimir.\n");
+/*  printf("Ex1, pedir um número inteiro e imprimir.\n");
     int inteiro;
     printf("Digite um número inteiro: \n");
 
     scanf("%d", &inteiro);
     printf("O número digitado foi: %d\n", inteiro);
-
+*/
     //Ex2 Digitar 3 valor e imprimir a soma
     int num1, num2, num3, total;
-    total = num1 + num2 + num3;
 
     printf("Digite o primeiro número: \n");
     scanf("%d", &num1);
 
     printf("Digite o segundo número: \n");
     scanf("%d", &num2);
+
     printf("Digite o terceiro número: \n");
     scanf("%d", &num3);
 
-    scanf("%d", &total);
+    total = num1 + num2 + num3;
 
     printf("A soma dos três números digitados é: %d\n", total);
-
+/*
     // Ex04 Ler um n real e imprimir o quadrado
     int numReal, quadrado;
 
@@ -38,7 +38,7 @@ int main()
     quadrado = numReal * numReal;
 
     printf("O quadrado do número digitado ( %d ) é: %d", numReal, quadrado);
-*/
+
     // Ex 05 Ler um número e imprimir a quinta parte dele
     float num1, quintaParte;
 
@@ -48,5 +48,6 @@ int main()
     quintaParte = num1 / 5;
 
     printf("A quinta parte de (%.0f) é: %.2f", num1, quintaParte);
+*/
     return 0;
 }
--- 01_variaveisExercios6-10/main.c
+++ 01_variaveisExercios6-10/main.c
@@ -5,7 +5,7 @@ int main()
 {
     setlocale(LC_ALL, "");
     // Ex 06 Converter Celsius para Fahrenheit
-
+/*
     float celsius, fahrenheit;
     printf("Diga a temperatura a ser convertida: \n");
 
@@ -13,5 +13,12 @@ int main()
     fahrenheit = celsius *(1.8) + 32.0;
 
     printf("A temperatura %.1f em Fahrenheit é %.1f", celsius, fahrenheit);
+*/
+    // Ex 07 converter Fahrenheit para Celsius
+    float celsius, fahrenheit;
+    printf("Diga a temperatura em Fahrenheit para converter para Celsius: \n");
+    scanf("%f", &fahrenheit);
+    celsius = 5.0 * (fahrenheit - 32.0) / 9.0;
+    printf("%.1f em Celsius é %.1f", fahrenheit, celsius);
     return 0;
 }
