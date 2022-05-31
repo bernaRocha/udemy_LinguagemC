12129f5848bf602f792c1092c6288885fa203926
Bernardo <mrochabernardo@gmail.com>
2022-05-31 11:18:15 -0300

    Até o exercício 20 pronto

--- 01_variaveisExercios11-20/main.c
+++ 01_variaveisExercios11-20/main.c
@@ -5,8 +5,9 @@
 int main()
 {
     setlocale(LC_ALL, "");
+/*
     // Ex 11 Leia uma velocidade em m/s e converta em km/h
-/*    float mSeg, kHora;
+    float mSeg, kHora;
     printf("Digite a velocifdade em m/s a ser convertida em km/h. \n");
     scanf("%f", &mSeg);
     kHora = mSeg * 3.6;
@@ -25,7 +26,7 @@ int main()
     scanf("%f", &kilometros);
     milhas = kilometros / 1.61 ; // Está arredondado
     printf("A distância %.1f kilometros em milhas é: %.1f", kilometros, milhas);
-*/
+
 
     // Ex 14 Leia um ângulo em graus e converta para radianos
     float graus, radianos;
@@ -34,4 +35,47 @@ int main()
     scanf("%f", &graus);
     radianos = graus * pi / 180;
     printf("%.4f graus em radianos é: %.4f ", graus, radianos);
+
+    // Ex 15 Leia um ângulo em radianos e converta para graus
+    float graus, radianos;
+    float pi = 3.141592;
+    printf("Apresente o ângulo em radianos e converta para graus: \n");
+    scanf("%f", &radianos);
+    graus = radianos * 180 / pi;
+    printf("%.2f radianos em graus é: %.2f", radianos, graus);
+
+    // Ex 16 Converta polegadas para centímetros
+    float polegadas, centimetros;
+    printf("Converta as polegadas em centímetros: \n");
+    scanf("%f", &polegadas);
+    centimetros = polegadas * 2.54;
+    printf("%.2f polegadas em centímetros é: %.2fcm", polegadas, centimetros);
+
+    // Ex 17 Converta centímetros em polegadas
+    float polegadas, centimetros;
+    printf("Converta centímetros em polegadas: \n");
+    scanf("%f", &centimetros);
+    polegadas = centimetros / 2.54;
+    printf("%.1f cm em polegadas é: %.4f", centimetros, polegadas);
+
+    // Ex 18 Converta um volume em m3 para litros
+    float litros, m3;
+    printf("Converta um volume em m3 para litros: \n");
+    scanf("%f", &m3);
+    litros = m3 * 1000;
+    printf("%.2f m3 em litros dá %.2f L", m3, litros);
+
+    // Ex 19 Converter litros em m3
+    float litros, m3;
+    printf("Converta um volume em litros para m3: \n");
+    scanf("%f", &litros);
+    m3 = litros / 1000;
+    printf("%.2f litros em m3 dá %.3f L", litros, m3);
+ */
+    //Ex 20 Converta Kg para Libras
+    float kg, lib;
+    printf("Converta a quantida em Kg para Libras: \n");
+    scanf("%f", &kg);
+    lib = kg / 0.45359237;
+    printf("%.2fKg em Libras dá: %.4fLb", kg, lib);
 }
