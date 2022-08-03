9f3d56c49320dfb64e6f45b3f3dbeac536f60d1a
Bernardo <mrochabernardo@gmail.com>
2022-08-02 21:08:01 -0300

    até o ex - 35 da seção 3 concluído

--- 01_variaveisExercios21_35/main.c
+++ 01_variaveisExercios21_35/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <math.h> // biblioteca para matemática
+#include <math.h>
 
 int main()
 {
@@ -19,59 +19,60 @@ int main()
     printf("Converta a medida de jardas para metros: \n");
     scanf("%f", &jardas);
     metros = 0.9144 * jardas;
-    printf("%.1f jardas em metros é: %.4f metros", jardas, metros);
+    printf("%.1f jardas em metros ï¿½: %.4f metros", jardas, metros);
 
     // Ex 23 Converta metros para jardas
     float jardas, metros;
     printf("Converta a medida em metros para jardas: \n");
     scanf("%f", &metros);
     jardas = metros / 0.9144;
-    printf("%.1f metros em jardas é %.4f", metros, jardas);
+    printf("%.1f metros em jardas ï¿½ %.4f", metros, jardas);
 
     // Ex 24 Converta m2 para acres
     float m2, acres;
     printf("Converta uma medida em m2 para acres: \n");
     scanf("%f", &m2);
     acres = m2 * 0.00024711;
-    printf("A medida %.2f m2 em acres é: %.8f", m2, acres);
+    printf("A medida %.2f m2 em acres ï¿½: %.8f", m2, acres);
 
-    // Ex 25 Converta uma área em acres para m2
+    // Ex 25 Converta uma ï¿½rea em acres para m2
     float m2, acres;
     printf("Converta uma medida em acres para m2: \n");
     scanf("%f", &acres);
     m2 = acres / 0.00024711;
-    printf("A área de %.1f acres em m2 é: %.2f", acres, m2);
+    printf("A ï¿½rea de %.1f acres em m2 ï¿½: %.2f", acres, m2);
 
     // Ex 26 Converter m2 para hectares
     float m2, hectares;
-    printf("Coverta a área de m2 para hectáres: \n");
+    printf("Coverta a ï¿½rea de m2 para hectï¿½res: \n");
     scanf("%f", &m2);
     hectares = m2 * 0.0001;
-    printf("A área de %.1fm2 convertida em hectares é: %.7f", m2, hectares);
+    printf("A ï¿½rea de %.1fm2 convertida em hectares ï¿½: %.7f", m2, hectares);
 
     // Ex 27 Converter hectares em m2
     float m2, hectares;
-    printf("Coverta a área de hectares para m2: \n");
+    printf("Coverta a ï¿½rea de hectares para m2: \n");
     scanf("%f", &hectares);
     m2 = hectares * 10000;
-    printf("%.1f hectares em m2 é: %.2f", hectares, m2);
+    printf("%.1f hectares em m2 ï¿½: %.2f", hectares, m2);
 
     // Ex 28 Ler 3 valores e apresentar a soma do quadrado dos 3
     int num1, num2, num3;
-    printf("Registre três valores e exiba a soma do quadrados deles -> \n");
+    printf("Registre trï¿½s valores e exiba a soma do quadrados deles -> \n");
     scanf("%d", &num1);
     scanf("%d", &num2);
     scanf("%d", &num3);
-    printf("A soma dos quadrados dos três números é: %d", (num1 * num1) + (num2 * num2)+ (num3 * num3));
-    printf("Ex 29 - Ler 4 notas e imprimir a média delas: \n");
+    printf("A soma dos quadrados dos trï¿½s nï¿½meros ï¿½: %d", (num1 * num1) + (num2 * num2)+ (num3 * num3));
+
+    printf("Ex 29 - Ler 4 notas e imprimir a mï¿½dia delas: \n");
 
     float num1, num2, num3, num4, media;
-    printf("Registre os 4 números: \n");
+    printf("Registre os 4 nï¿½meros: \n");
     scanf("%f%f%f%f", &num1, &num2, &num3, &num4);
 
     media = (num1 + num2 + num3 + num4) / 4;
 
-    printf("A média é %.2f", media);
+    printf("A mï¿½dia ï¿½ %.2f", media);
 
     printf("Ex 30 - Ler um valor real e a cotacao do dolar, apos isso o valor em dolares\n\n");
 
@@ -79,7 +80,7 @@ int main()
     printf("O valor em reais: \n");
     scanf("%f", &real);
 
-    printf("A cotação do dolar hoje: \n");
+    printf("A cotaï¿½ï¿½o do dolar hoje: \n");
     scanf("%f", &cotacao_dolar);
     real_em_dolar = real / cotacao_dolar;
     printf("O valor em reais convertido para dolares consiste em: %.2f", real_em_dolar);
@@ -92,7 +93,7 @@ int main()
     printf("A antecessor = %d\n", antecessor);
     sucessor = numero + 1;
     printf("O sucessor = %d\n", sucessor);
- */
+
     printf("Ex 32 - Ler um inteiro e imprimir a soma do sucessor de seu triplo\ncom o antecessor do dobro.\n");
     int numero, equacao;
     printf("Registre o inteiro: \n\n");
@@ -100,5 +101,37 @@ int main()
 
     equacao = (3 * numero + 1) + (2 * numero - 1);
 
-    printf("\nA soma do sucessor de seu triplo\ncom o antecessor do dobro = %d", equacao);
+    printf("\nA soma do sucessor de seu triplo\ncom o antecessor do dobro = %d\n", equacao);
+
+    printf("Ex 33 - Leia o lado de um quadrado e imprima a sua area.");
+    int lado, area;
+
+    printf("Diga o lado do quadrado para calculo da area: \n");
+    scanf("%d", &lado);
+
+    area = lado * lado;
+
+    printf("A area de um quadrado de lado %d e igual a %d", lado, area);
+
+    printf("Ex 34 - Leia um valor do raio e calcule a area do circulo. ");
+    float raio, pi = 3.141592, area_circulo;
+    printf("Qual o valor do raio? \n");
+    scanf("%f", &raio);
+
+    area_circulo = pi * raio * raio;
+
+    printf("\nA area do circulo de raio %.2f e %.2f", raio, area_circulo);
+ */
+
+    printf("Ex 35 - A partir dos catetos a e b, calcule a hipotenusa.\n");
+    float catetoA, catetoB;
+    float hipotenusa;
+    printf("Registre os catetos a e b: \n");
+    scanf("%f%f", &catetoA, &catetoB);
+    hipotenusa = sqrt ((catetoA * catetoA) + (catetoB * catetoB)); // SÃ³ funciona rodando pelo terminal:
+                                                                   // gcc main.c -lm # cria o arquivo a.out
+                                                                   // ./a.out                     
+    printf("A hipotenusa de um triangulo com lados de %.1f e %.1f e %.1f\n", catetoA, catetoB, hipotenusa);
+
+    return 0;
 }
