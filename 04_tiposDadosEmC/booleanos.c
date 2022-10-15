0ec031757668589c0a557ffd528de97d15634da0
Bernardo <mrochabernardo@gmail.com>
2022-10-15 09:05:05 -0300

    booleanos, código com comentários

--- 04_tiposDadosEmC/booleanos.c
+++ 04_tiposDadosEmC/booleanos.c
@@ -11,9 +11,14 @@ int main(){
         printf("It is false\n");
     }
 
-    printf("%d\n", a&&b);
-    printf("%d\n", a||b);
-    printf("%d\n", !b);
+    printf("Verdadeiro = %d\n", a);
+    printf("Falso = %d\n", b);
+    printf("Verdadeiro e falso dá = %d\n", a&&b);
+    // && (AND): takes 2 booleans; returns true only if both operands are true else false
+    printf("Verdadeiro ou falso dá = %d\n", a||b);
+    // || (OR): returns true if either or both of the operands are true else false
+    printf("Negação de falso dá = %d\n", !b);
+    // ! (NOT): takes 1 operand; return true if operand is false and false if operand is true
 
     return 0;
 }
