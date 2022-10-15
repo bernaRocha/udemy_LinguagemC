2114bce5f674ad88a4b8284fadab2ed86644f6c7
Bernardo <mrochabernardo@gmail.com>
2022-10-15 14:51:52 -0300

    exercícios

---
+++ 01_variaveisExercios/02_imprime-num-real.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+// leia um número real e o imprima
+    float numero;
+    printf("Digite um número real: ");
+    scanf("%f", &numero);
+    printf("O número digitado foi %f\n", numero);
+
+    return 0;
+}
